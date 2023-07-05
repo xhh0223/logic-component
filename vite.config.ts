ea890e3eef3f3cb8d905173919346ea02da2fc3d
xuhuihui <xhh0409@outlook.com>
2023-07-05 10:43:02 +0800

    feat(2)

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
 import { defineConfig } from "vite";
 import path from "path";
 import react from "@vitejs/plugin-react";
-
+import { visualizer } from 'rollup-plugin-visualizer'
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), visualizer(/* { open: true } */)],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
@@ -12,12 +12,19 @@ export default defineConfig({
   publicDir: "static",
   build: {
     lib: {
-      formats:['es'],
-      entry: path.resolve(__dirname, "src", "components"),
+      formats: ['es'],
+      entry: {
+        component: path.resolve(__dirname, "src", "components")
+      },
     },
     outDir: 'lib',
-    rollupOptions:{
-
+    rollupOptions: {
+      // external: ['react'],
+      output: {
+        chunkFileNames: '[name]-[hash].js', // 引入文件名的名称
+        entryFileNames: 'index.js', // 包的入口文件名称
+        assetFileNames: '[ext]/[name]-[hash].[ext]', // 资源文件像 字体，图片等
+      }
     }
   },
 });
