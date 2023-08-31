fc78f1a9cd09c1d64454459ececfbd958a414b30
xhh0223 <xhh0409@outlook.com>
2023-09-01 00:02:22 +0800

    docs: add select demo examples

--- src/Select/demo/singleDemo1.tsx
+++ src/Select/demo/singleDemo1.tsx
@@ -1,21 +1,45 @@
-import React, { SyntheticEvent, useCallback, useMemo, useState } from 'react'
+import React, { SyntheticEvent, useCallback, useMemo, useRef, useState } from 'react'
 import { Select, SelectItem, SelectRef } from '../src'
 
 const singleSelect = () => {
   const selectRef = useMemo(() => ({}) as SelectRef, [])
-  const [state, setState] = useState<any>()
+  const [state, setState] = useState({
+    curSelectValue: "",
+    repeatTriggerUnselected: true,
+  })
   const handleClick = useCallback<React.MouseEventHandler<HTMLDivElement>>((e) => {
     const { index } = e.currentTarget.dataset
     if (index) {
       selectRef.trigger(index)
     }
   }, [])
+
+  const selectRef2 = useRef<SelectRef>(null)
   return (
     <div>
-      <div>当前选中值{state}</div>
+      <Select
+        ref={selectRef2}
+        onChange={(v) => {
+          setState(preState => ({ ...preState, repeatTriggerUnselected: !!v }))
+        }}>
+        <div style={{ display: 'flex', gap: 20 }}>
+          <SelectItem id={"repeatTriggerUnselected"} value={true}>
+            <button onClick={() => {
+              selectRef2.current?.trigger("repeatTriggerUnselected")
+            }}>重复trigger取消选中</button>
+          </SelectItem>
+          <SelectItem id={"allowRepeatTrigger"} value={false}>
+            <button onClick={() => {
+              selectRef2.current?.trigger("allowRepeatTrigger")
+            }}>允许重复trigger</button>
+          </SelectItem>
+        </div>
+      </Select>
       <hr />
-      <Select ref={ref => Object.assign(selectRef, { ...ref })} onChange={v => {
-        setState(v)
+      <div>当前选中值{state.curSelectValue}</div>
+      <hr />
+      <Select repeatTriggerUnselected={state.repeatTriggerUnselected} ref={ref => Object.assign(selectRef, { ...ref })} onChange={v => {
+        setState(preState => ({ ...preState, curSelectValue: v as any }))
       }}>
         {Array.from({ length: 10 }).map((item, index) => {
           return <SelectItem key={index} id={`${index}`} value={index}>
