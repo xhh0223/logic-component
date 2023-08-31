2e9ba4da6b91c824e8af6da2f151eaf0ba584751
xhh0223 <xhh0409@outlook.com>
2023-08-31 22:54:56 +0800

    docs: add demo examples

---
+++ src/Select/demo/single.tsx
@@ -0,0 +1,21 @@
+import React, { useMemo, useRef } from 'react'
+import { Select, SelectItem, SelectRef } from '../src'
+
+const singleSelect = () => {
+  const selectRef = useMemo(() => ({}) as SelectRef, [])
+  return (
+    <div>
+      <Select ref={ref => Object.assign(selectRef, { ...ref })} onChange={v => {
+        console.log(v)
+      }}>
+        {Array.from({ length: 10 }).map((item, index) => {
+          return <SelectItem key={index} id={index} value={index}>
+            <div onClick={() => selectRef.trigger(index)}>{index}</div>
+          </SelectItem>
+        })}
+      </Select>
+    </div>
+  )
+}
+
+export default singleSelect
--- src/Select/index.md
+++ src/Select/index.md
@@ -1,6 +1,10 @@
 ---
 nav: component
+title: select
 ---
 <!-- 通过 code 标签配置 -->
+# 单选
+<code src="./demo/single.tsx"></code>
 
-<code src="./demo/index.tsx" description="demo 描述" >22</code>
+# 多选
+<code src="./demo/multiple.tsx"></code>
