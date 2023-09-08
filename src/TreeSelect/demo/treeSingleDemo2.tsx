c8832572fede72a4d81d9f1443dbc2da9502d939
xuhuihui <xhh0409@outlook.com>
2023-09-08 10:01:00 +0800

    docs: change treeSingleDemo2

--- src/TreeSelect/demo/treeSingleDemo2.tsx
+++ src/TreeSelect/demo/treeSingleDemo2.tsx
@@ -6,7 +6,7 @@ import './treeSingleDemo2.scss'
 
 const treeSingleDemo2 = () => {
   const classNamePrefix = "tree-single-demo2"
-  const treeData = useMemo(() => genTreeData(3, 2, 2), [])
+  const treeData = useMemo(() => genTreeData(5, 4, 4), [])
   const [levels, setLevels] = useState<TreeNode[][]>([treeData])
   const ref = useRef<TreeSelectSingleRef<string>>(null)
 
@@ -14,31 +14,32 @@ const treeSingleDemo2 = () => {
     <div className={`${classNamePrefix}`}>
       <TreeSelectSingle ref={ref} >
         {
-          levels.map(level => {
-            return level?.map((item, index) => {
-              return <div key={item.id} className={`${classNamePrefix}-group`}>
-                <TreeSelectItem id={`${item.id}`} parentId={item.parentId} value={item.value}>
-                  {
-                    ({ isChecked }) => {
-                      return <div className={`${classNamePrefix}-group-item`} onClick={() => {
-                        ref.current?.trigger(`${item.id}`).then((res) => {
-                          const curItemLevel = res?.path?.length
-                          if (item.children?.length && curItemLevel) {
-                            levels[curItemLevel] = item.children
-                            setLevels(res?.isChecked ? levels.slice(0, curItemLevel + 1) : levels.slice(0, curItemLevel))
-                          }
-                        })
-                      }}>
-                        <input checked={isChecked} type='checkbox' readOnly />
-                        {item.value}
-                      </div>
+          levels.map(level =>
+            <div className={`${classNamePrefix}-group`}>
+              {
+                level?.map((item) =>
+                  <TreeSelectItem key={item.id} id={`${item.id}`} parentId={item.parentId} value={item.value}>
+                    {
+                      ({ isChecked }) => {
+                        return <div className={`${classNamePrefix}-group-item`} onClick={() => {
+                          ref.current?.trigger(`${item.id}`).then((res) => {
+                            const curItemLevel = res?.level
+                            if (item.children?.length && curItemLevel) {
+                              levels[curItemLevel] = item.children
+                              setLevels(res?.isChecked ? levels.slice(0, curItemLevel + 1) : levels.slice(0, curItemLevel))
+                            }
+                          })
+                        }}>
+                          <input checked={isChecked} type='checkbox' readOnly />
+                          {item.value}
+                        </div>
+                      }
                     }
-                  }
-                </TreeSelectItem>
-              </div>
-            })
-          })
-        }
+                  </TreeSelectItem>)
+
+              }
+            </div>
+          )}
       </TreeSelectSingle>
     </div>
   )
