f8bf605aeaaa2dd0e50ddb2dc00f041ee240a24f
xuhuihui <xhh01854254@alibaba-inc.com>
2023-12-28 10:39:15 +0800

    perf: select-multiple | select-single

--- src/Select/src/select-multiple.tsx
+++ src/Select/src/select-multiple.tsx
@@ -2,114 +2,137 @@ import React, {
   type Ref,
   forwardRef,
   useImperativeHandle,
-  useMemo
-} from 'react'
-import { type Id, type SelectedValue } from './typing'
-import { Context, SelectContext } from './context'
-import { omit } from 'ramda'
+  useMemo,
+} from "react";
+import { type Id, type SelectedValue } from "./typing";
+import { Context, SelectContext } from "./context";
+import { omit, equals } from "ramda";
 
 export interface SelectMultipleProps {
-  children: React.ReactNode
+  children: React.ReactNode;
 }
 
 export interface SelectMultipleRef<ValueType> {
-  getAllValue: () => Array<SelectedValue<ValueType>>
-  getByIds: (id: Id[]) => Array<SelectedValue<ValueType>>
-  deleteByIds: (ids: Id[]) => void
-  reset: (ids?: Id[]) => Promise<void>
-  trigger: (selectedIds: Id[]) => Promise<Array<SelectedValue<ValueType>>>
+  getValuesByIds: (id: Id[]) => Array<SelectedValue<ValueType>>;
+  deleteByIds: (ids: Id[]) => void;
+  getIdsByValues: (value: ValueType[]) => Id[];
+  reset: (ids?: Id[]) => Promise<void>;
+  trigger: (selectedIds: Id[]) => Promise<Array<SelectedValue<ValueType>>>;
 }
 
 const InnerSelectMultiple = <ValueType,>(
   props: SelectMultipleProps,
   ref: Ref<SelectMultipleRef<ValueType>>
 ) => {
-  const { children } = props
+  const { children } = props;
 
-  const selectContext = useMemo(() => new Context<ValueType>(), [])
+  const selectContext = useMemo(() => new Context<ValueType>(), []);
 
   useImperativeHandle(
     ref,
     () => ({
-      getAllValue() {
-        const { getAllSelectItem } = selectContext
-        return getAllSelectItem().map((item) => (omit(['refreshHandler', 'repeatTriggerUnselected'], item)))
+      getIdsByValues(values) {
+        const { getAllSelectItem } = selectContext;
+        const allSelectItem = getAllSelectItem();
+        const ids: Id[] = [];
+        if (Array.isArray(values)) {
+          values?.forEach((value) => {
+            for (const item of allSelectItem) {
+              if (equals(item.value, value)) {
+                ids.push(item.id);
+                break;
+              }
+            }
+          });
+        }
+        return ids;
       },
-      getByIds(ids) {
-        const { getSelectItem } = selectContext
-        const items = ids?.map(id => omit(['refreshHandler', 'repeatTriggerUnselected'], getSelectItem(id)))
-        return items as Array<SelectedValue<ValueType>>
+      getValuesByIds(ids) {
+        const { getSelectItem } = selectContext;
+        const values: any[] = [];
+        if (Array.isArray(ids)) {
+          ids?.forEach((id) => {
+            const selectItem = getSelectItem(id);
+            if (selectItem) {
+              values.push(
+                omit(["refreshHandler", "repeatTriggerUnselected"], selectItem)
+              );
+            }
+          });
+          return values;
+        }
+        return values;
       },
       deleteByIds(ids) {
-        const { deleteSelectItem } = selectContext
+        const { deleteSelectItem } = selectContext;
         ids?.forEach((id) => {
-          deleteSelectItem(id)
-        })
+          deleteSelectItem(id);
+        });
       },
       async reset(ids) {
-        const { getAllSelectItem, getSelectItem } = selectContext
-        const allSelectItem = getAllSelectItem()
+        const { getAllSelectItem, getSelectItem } = selectContext;
+        const allSelectItem = getAllSelectItem();
         if (Array.isArray(ids)) {
           /** 多选 */
           ids?.forEach((id) => {
-            const selectItem = getSelectItem(id)
+            const selectItem = getSelectItem(id);
             if (selectItem && selectItem.isChecked) {
-              selectItem.isChecked = false
-              selectItem.refreshHandler()
+              selectItem.isChecked = false;
+              selectItem.refreshHandler();
             }
-          })
+          });
         } else {
           allSelectItem.forEach((item) => {
             if (item.isChecked) {
-              item.isChecked = false
-              item.refreshHandler()
+              item.isChecked = false;
+              item.refreshHandler();
             }
-          })
+          });
         }
       },
       async trigger(ids) {
-        const { getSelectItem } = selectContext
+        const { getSelectItem } = selectContext;
         if (Array.isArray(ids)) {
           /** 多选 */
-          const selectedItems: Array<SelectedValue<ValueType>> = []
+          const selectedItems: Array<SelectedValue<ValueType>> = [];
           ids?.forEach((id) => {
-            const selectItem = getSelectItem(id)
-            if (!selectItem) return
+            const selectItem = getSelectItem(id);
+            if (!selectItem) return;
 
             if (selectItem.repeatTriggerUnselected) {
-              selectItem.isChecked = !selectItem.isChecked
+              selectItem.isChecked = !selectItem.isChecked;
             } else {
-              selectItem.isChecked = true
+              selectItem.isChecked = true;
             }
             selectedItems.push({
               id: selectItem.id,
               value: selectItem.value,
-              isChecked: selectItem.isChecked
-            })
-          })
-          return selectedItems
+              isChecked: selectItem.isChecked,
+            });
+          });
+          return selectedItems;
         }
-        return []
-      }
+        return [];
+      },
     }),
     [selectContext]
-  )
+  );
   return (
     <SelectContext.Provider value={selectContext}>
       {children}
     </SelectContext.Provider>
-  )
-}
+  );
+};
 
 type ForwardRefReturnType<Value> = ReturnType<
   typeof forwardRef<SelectMultipleRef<Value>, SelectMultipleProps>
->
+>;
 
 /** 保留多选泛型 */
 type InnerSelectMultipleType = <Value>(
   ...params: Parameters<ForwardRefReturnType<Value>>
-) => ReturnType<ForwardRefReturnType<Value>>
+) => ReturnType<ForwardRefReturnType<Value>>;
 
 export const SelectMultiple = forwardRef(
   InnerSelectMultiple
-) as InnerSelectMultipleType
+) as InnerSelectMultipleType;
--- src/Select/src/select-single.tsx
+++ src/Select/src/select-single.tsx
@@ -1,71 +1,98 @@
-import React, { type Ref, forwardRef, useImperativeHandle, useMemo } from 'react'
-import { type Id, type SelectedValue } from './typing'
-import { Context, SelectContext } from './context'
-
+import React, {
+  type Ref,
+  forwardRef,
+  useImperativeHandle,
+  useMemo,
+} from "react";
+import { type Id, type SelectedValue } from "./typing";
+import { Context, SelectContext } from "./context";
+import { equals } from "ramda";
 export interface SelectSingleProps {
-  children: React.ReactNode
+  children: React.ReactNode;
 }
 
 export interface SelectSingleRef<ValueType> {
-  reset: () => Promise<void>
-  trigger: (selectedId: Id) => Promise<SelectedValue<ValueType> | undefined>
+  getIdByValue: (value: ValueType) => Id | undefined;
+  reset: () => Promise<void>;
+  trigger: (selectedId: Id) => Promise<SelectedValue<ValueType> | undefined>;
 }
 
-const InnerSelectSingle = <ValueType,>(props: SelectSingleProps, ref: Ref<SelectSingleRef<ValueType>>) => {
-  const { children } = props
-  const selectContext = useMemo(() => new Context<ValueType>(), [])
+const InnerSelectSingle = <ValueType,>(
+  props: SelectSingleProps,
+  ref: Ref<SelectSingleRef<ValueType>>
+) => {
+  const { children } = props;
+  const selectContext = useMemo(() => new Context<ValueType>(), []);
 
-  useImperativeHandle(ref, () => ({
-    async reset () {
-      const { getAllSelectItem } = selectContext
-      for (const item of getAllSelectItem()) {
-        if (item.isChecked) {
-          item.isChecked = false
-          item.refreshHandler()
-          break
+  useImperativeHandle(
+    ref,
+    () => ({
+      getIdByValue(value) {
+        const { getAllSelectItem } = selectContext;
+        for (const item of getAllSelectItem()) {
+          if (equals(item.value, value)) {
+            return item.id;
+          }
         }
-      }
-    },
-    async trigger (id) {
-      const { getAllSelectItem, getSelectItem } = selectContext
-      const selectedItem = getSelectItem(id)
-      if (!selectedItem) {
-        console.error('selectItem的id不存在')
-        return
-      }
-
-      for (const item of getAllSelectItem()) {
-        if (item.isChecked && item.id !== id) {
-          item.isChecked = false
-          item.refreshHandler()
-          break
+      },
+      async reset() {
+        const { getAllSelectItem } = selectContext;
+        for (const item of getAllSelectItem()) {
+          if (item.isChecked) {
+            item.isChecked = false;
+            item.refreshHandler();
+            break;
+          }
         }
-      }
+      },
+      async trigger(id) {
+        const { getAllSelectItem, getSelectItem } = selectContext;
+        const selectedItem = getSelectItem(id);
+        if (!selectedItem) {
+          console.error("selectItem的id不存在");
+          return;
+        }
 
-      if (selectedItem.repeatTriggerUnselected) {
-        selectedItem.isChecked = !selectedItem.isChecked
-      } else {
-        selectedItem.isChecked = true
-      }
-      selectedItem.refreshHandler()
-      return {
-        isChecked: selectedItem.isChecked,
-        id: selectedItem.id,
-        value: selectedItem.value
-      }
-    }
-  }), [selectContext])
+        for (const item of getAllSelectItem()) {
+          if (item.isChecked && item.id !== id) {
+            item.isChecked = false;
+            item.refreshHandler();
+            break;
+          }
+        }
 
-  return <SelectContext.Provider value={selectContext}>
-    {children}
-  </SelectContext.Provider>
-}
+        if (selectedItem.repeatTriggerUnselected) {
+          selectedItem.isChecked = !selectedItem.isChecked;
+        } else {
+          selectedItem.isChecked = true;
+        }
+        selectedItem.refreshHandler();
+        return {
+          isChecked: selectedItem.isChecked,
+          id: selectedItem.id,
+          value: selectedItem.value,
+        };
+      },
+    }),
+    [selectContext]
+  );
 
-type ForwardRefReturnType<Value> = ReturnType<typeof forwardRef<SelectSingleRef<Value>, SelectSingleProps>>
+  return (
+    <SelectContext.Provider value={selectContext}>
+      {children}
+    </SelectContext.Provider>
+  );
+};
+
+type ForwardRefReturnType<Value> = ReturnType<
+  typeof forwardRef<SelectSingleRef<Value>, SelectSingleProps>
+>;
 
 /** 保留单选泛型 */
-type InnerSelectSingleType = <Value, >(
+type InnerSelectSingleType = <Value>(
   ...params: Parameters<ForwardRefReturnType<Value>>
-) => ReturnType<ForwardRefReturnType<Value>>
+) => ReturnType<ForwardRefReturnType<Value>>;
 
-export const SelectSingle = forwardRef(InnerSelectSingle) as InnerSelectSingleType
+export const SelectSingle = forwardRef(
+  InnerSelectSingle
+) as InnerSelectSingleType;
