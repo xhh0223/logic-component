9807ab305c1efe425ebdf44bf84b35fc4bfbae87
xuhuihui <xhh01854254@alibaba-inc.com>
2024-04-29 22:58:43 +0800

    feat

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,41 +1,9 @@
-/**
- * @type {import('eslint').ESLint.ConfigData}
- */
-module.exports = {
-  "env": {
-    "browser": true,
-    "es2021": true
-  },
-  "extends": [
-    "standard-with-typescript",
-    "plugin:react/recommended",
-    "prettier",
-  ],
-  "overrides": [
-    {
-      "env": {
-        "node": true
-      },
-      "files": [
-        ".eslintrc.{js,cjs}"
-      ],
-      "parserOptions": {
-        "sourceType": "script"
-      }
-    }
-  ],
-  "parserOptions": {
-    "ecmaVersion": "latest",
-    "sourceType": "module",
-  },
-  "plugins": [
-    "react",
-    "react-hooks"
-  ],
-  "rules": {
-    "@typescript-eslint/explicit-function-return-type": "off",
-    "@typescript-eslint/strict-boolean-expressions": "off",
-    "@typescript-eslint/no-floating-promises": "off",
-    "react-hooks/exhaustive-deps": "warn"
-  }
-}
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+
+export default [
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+];
