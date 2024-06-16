7e00020d372f5cb5c41640afffdf1df9cfa7f4f9
xuhuihui <xhh01854254@alibaba-inc.com>
2024-06-16 18:01:37 +0800

    feat(change docs): update docs

--- packages/docs/src/page/index/index.tsx
+++ packages/docs/src/page/index/index.tsx
@@ -8,7 +8,12 @@ const Index = () => {
     <>
       <Flex align="center">
         <h1>logic-component</h1>
-        <a target="" style={{ marginLeft: 'auto' }}>
+        <a
+          target="_blank"
+          href="https://github.com/xhh0223/logic-component"
+          style={{ marginLeft: 'auto' }}
+          rel="noreferrer"
+        >
           github
         </a>
       </Flex>
