f0368e258c098b491fe38d6d89f46df8134f1e22
xuhuihui <xhh01854254@alibaba-inc.com>
2024-07-04 21:00:09 +0800

    feat: 补充样式

--- packages/docs/src/layout/nav/index.tsx
+++ packages/docs/src/layout/nav/index.tsx
@@ -1,15 +1,16 @@
+import './index.scss'
+
 import { Flex } from 'antd'
 
 import { MobileMenu } from '../mobile-menu'
-
 export const Nav = () => {
   return (
-    <Flex align="center" style={{ height: 40 }}>
+    <Flex className="nav" align="center" justify="space-between">
       <Flex component={'h2'} align="center" gap={4}>
         <img style={{ width: 20, height: 20 }} src={'/logic-component/logic.svg'} />
         <div>logic-component</div>
       </Flex>
-      <h3 style={{ marginLeft: 'auto' }}>
+      <h3 className="nav-github">
         <a target="_blank" href="https://github.com/xhh0223/logic-component" rel="noreferrer">
           Github
         </a>
